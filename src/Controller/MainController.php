fa40d6adf8f3d6d9bc697b9155487eca0244ca78
Viktoriia <vikaveris@ya.ru>
2019-08-19 01:08:18 +0300

    Add table view

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Employees;
 
 class MainController extends AbstractController
 {
@@ -12,8 +13,13 @@ class MainController extends AbstractController
      */
     public function index()
     {
+        $em = $this->getDoctrine()->getManager(); // Entity Manager for DB
+
+        $employees = $em->getRepository(Employees::class)->findAll();
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'employees' => $employees
         ]);
     }
 }
