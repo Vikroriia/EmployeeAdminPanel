f5c2eb592d725dd0e18bedd21d498d057c18c7e8
Viktoriia <vikaveris@ya.ru>
2019-08-21 01:05:51 +0300

    Add table view, buttons for actions, action "Delete", modal form for "Add new"

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Positions;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Employees;
+use App\Repository\EmployeesRepository;
 
 class MainController extends AbstractController
 {
@@ -16,10 +19,24 @@ class MainController extends AbstractController
         $em = $this->getDoctrine()->getManager(); // Entity Manager for DB
 
         $employees = $em->getRepository(Employees::class)->findAll();
+        $positions = $em->getRepository(Positions::class)->findAll();
 
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
-            'employees' => $employees
+            'employees' => $employees,
+            'positions' => $positions
         ]);
+    }
+
+    /**
+     * @Route("remove/{employee}", name="remove_employee")
+     */
+    public function removeEmployee(Employees $employee)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($employee);
+        $em->flush();
+
+        return $this->redirectToRoute("index");
     }
 }
