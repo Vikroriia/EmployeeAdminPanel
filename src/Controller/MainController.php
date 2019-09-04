e3561dd94edffbe0c45235ba51533c07babc873d
Viktoriia <vikaveris@ya.ru>
2019-09-04 22:59:51 +0300

    Add table view, buttons for actions, action "Delete", modal form for "Add new"

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,28 +3,34 @@
 namespace App\Controller;
 
 use App\Entity\Positions;
-use http\Env\Request;
+use App\Form\EmployeeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Employees;
-use App\Repository\EmployeesRepository;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index()
+    public function index(Request $request)
     {
         $em = $this->getDoctrine()->getManager(); // Entity Manager for DB
 
-        $employees = $em->getRepository(Employees::class)->findAll();
-        $positions = $em->getRepository(Positions::class)->findAll();
+       // $employees = $em->getRepository(Employees::class)->findAll();
+      //  $positions = $em->getRepository(Positions::class)->findAll();
+        
+        $employee = new Employees();
+        $formAddNew = $this->createForm(EmployeesType::class, $employee);
+        $formAddNew->handleRequest($request);
+
 
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
-            'employees' => $employees,
-            'positions' => $positions
+            //'employees' => $employees,
+            //'positions' => $positions,
+            'formAddNew' => $formAddNew->createView()
         ]);
     }
 
@@ -35,6 +41,20 @@ class MainController extends AbstractController
     {
         $em = $this->getDoctrine()->getManager();
         $em->remove($employee);
+        $em->flush();
+
+        return $this->redirectToRoute("index");
+    }
+
+    /**
+     * @Route("add_new/{employee}", name="add_new_employee")
+     */
+    public function addNewEmployee(Employees $employee)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $employee = new Employees();
+        $formAddNew = $this->createForm(EmployeeType::class, $employee);
+
         $em->flush();
 
         return $this->redirectToRoute("index");
